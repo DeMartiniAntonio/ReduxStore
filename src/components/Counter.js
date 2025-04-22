d646f0ccda359f9d971fb1ee6cfbf8639b06a601
Antonio <antonio.sluzbeni@gmail.com>
2025-04-22 20:49:07 +0200

    Authentication

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,34 @@
 import classes from './Counter.module.css';
 import { useSelector, useDispatch } from 'react-redux';
-
+import { counterActions } from '../store/counter';
 const Counter = () => {
-  const counter = useSelector(state=>state.counter);  
+  const counter = useSelector((state)=>state.counter.counter);  
   const dispatch=useDispatch();
+  const show = useSelector((state)=>state.counter.showCounter);
+
   const incrementHandler = () =>{
-    dispatch({type: 'increment'});
+    dispatch(counterActions.increment());
+  };
+  const increaseHandler = () =>{
+    dispatch(counterActions.increase(10));
   };
   const decrementHandler = () =>{
-    dispatch({type: 'decrement'});
+    dispatch(counterActions.decrement());
   };
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter())
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show &&<div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>
           Increment
+        </button>
+        <button onClick={increaseHandler}>
+          Increase by 5
         </button>
         <button onClick={decrementHandler}>
           Decrement
